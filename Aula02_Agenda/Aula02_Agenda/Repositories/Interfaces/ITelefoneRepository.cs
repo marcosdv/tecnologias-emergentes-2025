0634c6853f117169588c97278e7d2fa7199343df
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2025-08-13 22:11:14 -0300

    Aula03 - 13-08-2025

---
+++ Aula02_Agenda/Aula02_Agenda/Repositories/Interfaces/ITelefoneRepository.cs
@@ -0,0 +1,13 @@
+﻿using Aula02_Agenda.Models;
+
+namespace Aula02_Agenda.Repositories.Interfaces;
+
+public interface ITelefoneRepository
+{
+    IEnumerable<Telefone> Get();
+    Telefone? GetById(int id);
+
+    void Adicionar(Telefone telefone);
+    void Alterar(Telefone telefone);
+    void Excluir(int id);
+}
