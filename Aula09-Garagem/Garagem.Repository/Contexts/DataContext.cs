fce770e70ab189940f32c38aed505eb77a118794
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2025-10-22 22:09:03 -0300

    Aula10 - 22-10-2025

---
+++ Aula09-Garagem/Garagem.Repository/Contexts/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Garagem.Application.Entities;
+using Garagem.Repository.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garagem.Repository.Contexts;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> opt) : base(opt)
+    {
+    }
+
+    public DbSet<Modelo> Modelos { get; set; }
+    public DbSet<Veiculo> Veiculos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new ModeloMap());
+        modelBuilder.ApplyConfiguration(new VeiculoMap());
+    }
+}
