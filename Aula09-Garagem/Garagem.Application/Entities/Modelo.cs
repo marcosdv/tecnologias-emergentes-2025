2359709958111c41331362421e78d70b2321f430
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2025-11-05 22:08:11 -0300

    Aula12 - 05-11-2025

--- Aula09-Garagem/Garagem.Application/Entities/Modelo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Garagem.Application.Entities;
-
-public class Modelo : Entity
-{
-    public string Marca { get; set; } = string.Empty;
-    public string Nome { get; set; } = string.Empty;
-    public List<Veiculo> Veiculos { get; set; } = [];
-}
--- Aula09-Garagem/Garagem.Application/Services/Interfaces/IModeloService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Garagem.Application.Commands;
-using Garagem.Application.Commands.Modelo;
-
-namespace Garagem.Application.Services.Interfaces;
-
-public interface IModeloService
-{
-    CommandResult Adicionar(ModeloAdicionarCommand command);
-}
